d8e7b59a4f2efc0158188a3465fe77e83e08dc36
RyouYatTung <leungyattung1108@gmail.com>
2023-06-29 00:16:04 +0900

    push

---
+++ js/es7.js
@@ -0,0 +1,16 @@
+//es7
+// .includes()
+// **
+
+const pets = ["cat", "dog", "bat"];
+pets.includes("dog");
+
+const square = (x) => x ** 2;
+const cube = (y) => y ** 3;
+
+//es8
+// .padStart()
+// .padEnd()
+
+"turtle".padStart(10);
+"turtle".padEnd(10);
